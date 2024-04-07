abafc189b47e1aac2b2ab65db17f2b6749f3501c
kaitolorenzo03 <kaitolorenzo03@correo.ugr.es>
2024-04-07 18:28:49 +0200

    a

--- ejerciciosThree/ejercicio5/original.js
+++ ejerciciosThree/ejercicio5/original.js
@@ -9,20 +9,27 @@ class MyOriginal extends THREE.Object3D {
         Mat.needsUpdate = true;
 
 		//Se crean los objetos
-        var cubo_ext = new THREE.BoxGeometry(5,7,5);
-        var cilindro_interior = new THREE.CylinderGeometry(2,2,7,32);
-        var esfera = new THREE.SphereGeometry(2,32);
+        var cubo_ext = new THREE.BoxGeometry(5,5,5);
+        var cilindro_interior = new THREE.CylinderGeometry(2,2,7.3,32);
+        var esfera = new THREE.SphereGeometry(3,32);
+        var torus = new THREE.TorusGeometry(2,0.5,16,100);
         
        
-        esfera.translate(0.5,-0.5);     
+        esfera.translate(0,-0.45,0);  
+        torus.rotateX(Math.PI/2);
+        torus.translate(3,2,0);  
         // Se crean los nodos Mesh
+       
         var cubito = new THREE.Mesh(cubo_ext, Mat);
         var cilindro_interior_M = new THREE.Mesh(cilindro_interior, Mat);
         var esferita = new THREE.Mesh(esfera, Mat);
+        var torus_M = new THREE.Mesh(torus, Mat);
     
         var csg = new CSG();
-        csg.union([cubito,cilindro_interior_M]);
+        csg.union([torus_M,cubito]);
+        csg.subtract([cubito,cilindro_interior_M]);
         csg.subtract([esferita]);
+        
 
         this.original = csg.toMesh();
 
@@ -36,8 +43,8 @@ class MyOriginal extends THREE.Object3D {
   }
 
   update () {
-    this.original.rotation.y += 0.01;
-    this.original.rotation.x += 0.01;
+    //this.original.rotation.y += 0.01;
+    //this.original.rotation.x += 0.01;
   }
 }
 
